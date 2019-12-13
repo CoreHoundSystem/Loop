a73ad771175030b88c96326c179ad11e7a056723
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-12-13 11:38:49 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,10 +1,30 @@
 lCal='corehoundsystem.com_m7ur5vu1etheie89iqesqbovfo@group.calendar.google.com';
+core='info@corehoundsystem.com';
 gmail='info@thesassyalpaca.com';
 relIDs='monday,thursday,fishing,songs';
 u='monday';
 
 
 function checkCalendar(u) {
+	gapi.client.calendar.events.insert({
+		'calendarId': lCal,
+		"end": {
+			'dateTime':'2019-12-20T14:00:00-08:00'
+		},
+		'start': {
+			'dateTime':'2019-12-20T13:00:00-08:00'
+		},
+		'description':'Test Event Share',
+		'attendees':[
+			{'email':core}
+		],
+		
+	}).then(function(response) {
+		console.log(response);
+	});
+}
+	
+	/*
 	gapi.client.calendar.acl.insert({
 		'calendarId': lCal,
 		'resource':{
@@ -18,6 +38,7 @@ function checkCalendar(u) {
 		console.log(response);
 	});
 }
+*/
 	/*
 	see if the user has a calendar with specific name
 		yes-proceed
