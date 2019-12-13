61d9eda8c5017ddb024c2938c8a6482291f40d0d
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2019-12-13 10:44:47 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,13 +1,14 @@
 lCal='corehoundsystem.com_m7ur5vu1etheie89iqesqbovfo@group.calendar.google.com';
 relIDs='monday,thursday,fishing,songs';
+u='monday';
 
-function checkCalendar() {
+function checkCalendar(u) {
 	gapi.client.calendar.events.list({
 		'calendarId': lCal,
 		'timeMin': (new Date()).toISOString(),
 		'showDeleted': false,
 		'singleEvents': true,
-		'q':relIDs,
+		'q':u,
 		'maxResults': 10,
 		'orderBy': 'startTime'
 	}).then(function(response) {
@@ -25,5 +26,23 @@ function checkCalendar() {
 		} else {
 			console.log('No upcoming events found.');
 		}
+	});
+	addEvent();
+}
+
+function addEvent() {
+	gapi.client.calendar.events.insert({
+		'calendarId': lCal,
+		"end": {
+			'dateTime':'2019-12-20T14:00:00-08:00'
+		},
+		'start': {
+			'dateTime':'2019-12-20T13:00:00-08:00'
+		},
+		'description':'Test Event'
+		
+		
+	}).then(function(response) {
+		console.log(response);
 	});
 }
